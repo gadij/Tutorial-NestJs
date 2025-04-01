183bdfbf64bfb52584982e4c0b085279c839b22a
gadij <gadij00@gmail.com>
2025-04-01 17:41:10 +0300

    saving signed up user to the db and return the user created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,46 +8,51 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
+    // console.log('dto', dto);
     const hashPassword = await argon.hash(dto.password);
-    try {
-      const user = await this.prisma.user.create({
-        data: {
-          email: dto.email,
-          hash: hashPassword,
-        },
-      });
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { hash, ...result } = user;
-
-      return result;
-    } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === 'P2002') {
-          throw new ForbiddenException('Credentials already exist');
-        }
-      }
-      throw error;
-    }
-  }
-
-  async signin(dto: AuthDto) {
-    const user = await this.prisma.user.findUnique({
-      where: {
+    // try {
+    //   console.log('this.prisma', this.prisma);
+    const user = await this.prisma.user.create({
+      data: {
         email: dto.email,
+        hash: hashPassword,
       },
     });
 
-    if (!user) {
-      throw new ForbiddenException('credentials are invalid');
-    }
+    return user;
+    //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    //   const { hash, ...result } = user;
 
-    const pwMatches = await argon.verify(user.hash, dto.password);
-    if (!pwMatches) {
-      throw new ForbiddenException('credentials are invalid');
-    }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { hash, ...result } = user;
-    return result;
+    //   return result;
+    // } catch (error) {
+    //   if (error instanceof PrismaClientKnownRequestError) {
+    //     if (error.code === 'P2002') {
+    //       throw new ForbiddenException('Credentials already exist');
+    //     }
+    //   }
+    //   throw error;
+    // }
+    // return { msg: 'I am signup' };
+  }
+
+  signin(dto: AuthDto) {
+    // const user = await this.prisma.user.findUnique({
+    //   where: {
+    //     email: dto.email,
+    //   },
+    // });
+
+    // if (!user) {
+    //   throw new ForbiddenException('credentials are invalid');
+    // }
+
+    // const pwMatches = await argon.verify(user.hash, dto.password);
+    // if (!pwMatches) {
+    //   throw new ForbiddenException('credentials are invalid');
+    // }
+    // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // const { hash, ...result } = user;
+    // return result;
+    return { msg: 'I am signin' };
   }
 }
